43e7f0765230704afa2f92e2300a28099b628575
Nathan Ladd <nathanladd@gmail.com>
2020-11-05 10:06:16 -0600

    Logging

--- lib/aggregate_streams/handle.rb
+++ lib/aggregate_streams/handle.rb
@@ -7,6 +7,8 @@ module AggregateStreams
         include Messaging::Handle
         include Messaging::StreamName
 
+        include Log::Dependency
+
         prepend Configure
 
         extend StoreClass
@@ -26,11 +28,16 @@ module AggregateStreams
     end
 
     def handle(message_data)
+      logger.trace { "Handling message (Stream: #{message_data.stream_name}, Global Position: #{message_data.global_position})" }
+
       stream_id = Messaging::StreamName.get_id(message_data.stream_name)
 
       aggregation, version = store.fetch(stream_id, include: :version)
 
-      return if aggregation.processed?(message_data)
+      if aggregation.processed?(message_data)
+        logger.info(tag: :ignored) { "Message already handled (Stream: #{message_data.stream_name}, Global Position: #{message_data.global_position})" }
+        return
+      end
 
       input_metadata = Messaging::Message::Metadata.build(message_data.metadata)
       output_metadata = Messaging::Message::Metadata.build
@@ -51,7 +58,17 @@ module AggregateStreams
       if write_message_data
         assure_message_data(write_message_data)
       else
+        logger.info(tag: :ignored) { "Message ignored (Stream: #{message_data.stream_name}, Global Position: #{message_data.global_position})" }
         return
+      end
+
+      logger.info do
+        message_type = message_data.type
+        unless write_message_data.type == message_type
+          message_type = "#{write_message_data.type} ← #{message_type}"
+        end
+
+        "Message copied (Message Type: #{message_type}, Stream: #{message_data.stream_name}, Global Position: #{message_data.global_position})"
       end
 
       Try.(MessageStore::ExpectedVersion::Error) do
--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+LOG_TAGS=${LOG_TAGS:-ignored,_untagged}
+
+export LOG_TAGS
+
 ruby \
   --disable-gems \
   test/automated.rb \
